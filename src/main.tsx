89e3aa4042236d23b0e5ddd6ec503d35e8e1fb49
unknown <cse12005038brur@gmail.com>
2025-12-20 19:10:52 +0600

    modified the font family

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,11 @@ import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./redux/store.ts";
 import { Toaster } from "@/components/ui/sonner";
 
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
